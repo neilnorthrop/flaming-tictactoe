54512b49f2acd80ac3bcb7591fdebf8313575351
Neil Northrop <nnorthrop@gmail.com>
2014-08-01 16:09:22 -0700

    add a choice to the main page to who goes first.

--- tictactoe.rb
+++ tictactoe.rb
@@ -9,6 +9,18 @@ set :computer_ai, ComputerAI
 
 get '/' do
 	settings.game.reset_board
+	haml :game
+end
+
+post '/decide' do
+	if params[:message] == 'computer'
+		redirect '/computer'
+	elsif params[:message] == 'player'
+		redirect '/index'
+	end	
+end
+
+get '/index' do
 	haml :index
 end
 
@@ -18,4 +30,16 @@ post '/index' do
 	settings.computer_ai.computer_turn(settings.game)
 	settings.game.check_game
 	haml :index
+end
+
+get '/computer' do
+	haml :computer
+end
+
+post '/computer' do
+	settings.computer_ai.computer_turn(settings.game)
+	settings.game.check_game
+	settings.game.set_position(params[:message].to_i, "X")
+	settings.game.check_game
+	haml :computer
 end
