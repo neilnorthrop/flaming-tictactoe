a4132a9da6c515a927a870841b1738da386f28b7
Neil Northrop <nnorthrop@gmail.com>
2014-08-12 14:38:23 -0700

    add a check_game to find a winner, a game_over? boolean, and moves for X and O.

--- boardgame4x4.rb
+++ boardgame4x4.rb
@@ -7,7 +7,7 @@ class BoardGame
   def initialize
     @board = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
     @immutable_board = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
-    @winning_positions = []
+    @winning_positions = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16], [1, 5, 9, 13], [2, 6, 10, 14], [3, 7, 11, 15], [4, 8, 12, 16], [1, 6, 11, 16], [4, 7, 10, 13]]
   end
   
   def display
@@ -38,6 +38,33 @@ class BoardGame
     moves_collected = []
     @board.each.with_index { |v,k| moves_collected << @immutable_board[k] if v == letter }
     return moves_collected
+  end
+  
+  def tally_moves_remaining
+    moves_remaining = []
+    @board.each { |position| moves_remaining << position if position !~ /X|O/ }
+    return moves_remaining
+  end
+  
+  def x_moves
+    moves("X")
+  end
+  
+  def o_moves
+    moves("O")
+  end
+  
+  def check_game
+    @winning_positions.each do |row|
+      return :player_x if (row - x_moves).empty?
+      return :player_o if (row - o_moves).empty?
+    end
+    return :draw if tally_moves_remaining.empty?
+    return :nobody
+  end
+  
+  def game_over?
+    check_game != :nobody
   end
 end
 
@@ -129,6 +156,77 @@ if __FILE__==$0
       @test_game.set_position(4, "O")
       assert_equal [1, 3], @test_game.moves("X")
       assert_equal [2, 4], @test_game.moves("O")
+    end
+    
+    def test_that_board_tallies_moves_remaining
+      @test_game.set_position(1, "X")
+      @test_game.set_position(2, "X")
+      @test_game.set_position(3, "X")
+      @test_game.set_position(4, "X")
+      @test_game.set_position(5, "X")
+      @test_game.set_position(6, "X")
+      assert_equal [7, 8, 9, 10, 11, 12, 13, 14, 15, 16], @test_game.tally_moves_remaining
+    end
+
+    def test_player_moves_returns_all_moves_for_player_X
+      @test_game.set_position(1, "X")
+      @test_game.set_position(2, "X")
+      @test_game.set_position(3, "X")
+      @test_game.set_position(4, "X")
+      assert_equal [1, 2, 3, 4], @test_game.x_moves
+    end
+
+    def test_player_moves_returns_all_moves_for_player_O
+      @test_game.set_position(1, "O")
+      @test_game.set_position(2, "O")
+      @test_game.set_position(3, "O")
+      @test_game.set_position(4, "O")
+      assert_equal [1, 2, 3, 4], @test_game.o_moves
+    end
+    
+    def test_board_responds_to_check_game
+      assert_equal :nobody, @test_game.check_game
+    end
+    
+    def test_that_check_game_returns_player_x_symbol_when_player_X_wins
+      @test_game.set_position(1, "X")
+      @test_game.set_position(2, "X")
+      @test_game.set_position(3, "X")
+      @test_game.set_position(4, "X")
+      assert_equal :player_x, @test_game.check_game
+    end
+    
+    def test_that_check_game_returns_player_o_symbol_when_player_O_wins
+      @test_game.set_position(1, "O")
+      @test_game.set_position(2, "O")
+      @test_game.set_position(3, "O")
+      @test_game.set_position(4, "O")
+      assert_equal :player_o, @test_game.check_game
+    end
+    
+    def test_that_check_game_returns_draw_symbol_when_nobody_wins
+      @test_game.board = ["X", "O", "X", "O", "O", "X", "O", "X", "O", "X", "O", "X", "X", "O", "X", "O"]
+      assert_equal :draw, @test_game.check_game
+    end
+    
+    def test_that_game_over_returns_false_if_no_player_wins
+      refute @test_game.game_over?
+    end
+    
+    def test_that_game_over_returns_true_if_a_player_O_wins
+      @test_game.set_position(1, "O")
+      @test_game.set_position(2, "O")
+      @test_game.set_position(3, "O")
+      @test_game.set_position(4, "O")
+      assert @test_game.game_over?
+    end
+    
+    def test_that_game_over_returns_true_if_a_player_X_wins
+      @test_game.set_position(1, "X")
+      @test_game.set_position(2, "X")
+      @test_game.set_position(3, "X")
+      @test_game.set_position(4, "X")
+      assert @test_game.game_over?
     end
   end
 end
