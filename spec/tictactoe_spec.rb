1044d3de7dec8649dae530ac5fad9d29c03e8017
Neil Northrop <nnorthrop@gmail.com>
2014-09-30 17:25:05 -0700

    add id's to board.haml cells. add test to make sure you can post a move to the board.

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -14,6 +14,10 @@ def get_app
 	expect(last_response).to be_ok
 end
 
+def capy_app
+	Capybara.app = Sinatra::Application.new
+end
+
 RSpec.describe "when visiting the website" do
 	def app
 		Sinatra::Application
@@ -26,12 +30,54 @@ RSpec.describe "when visiting the website" do
 	end
 end
 
-RSpec.describe "when player one picks the 3x3 option" do
-	Capybara.app = Sinatra::Application.new
+RSpec.describe "when player one picks the 3x3 or 4x4 option" do
+	capy_app
 
 	it "shows a 3x3 board" do
 		visit '/'
 		choose '3x3'
 		click_button('Computer')
+		expect(page).to have_css('.board')
+	end
+
+	it "shows a 4x4 board" do
+		visit '/'
+		choose '4x4'
+		click_button('Player')
+		expect(page).to have_css('.board4x4')
 	end
 end
+
+RSpec.describe "when player one picks a cell" do
+	capy_app
+
+	it "posts a move to the game board" do
+		visit '/'
+		choose '3x3'
+		click_button('Computer')
+		click_button('1')
+		expect(page).to have_css('#X')
+	end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
